f8f23777bd0bd42a4202c4950b7847fa79b978cc
Anatoliy <toliankir@gmail.com>
2020-02-25 20:51:18 +0200

    modified:   index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ async function test() {
     const promGoods = await promClient.getAllGoods();
     const ostatkiGoods = await ostatki.getGoods();
     const diffGoods = [];
+    let arr = [];
     promGoods.forEach((promEl) => {
         const fromOst = ostatkiGoods.find(ostEl => promEl.name.indexOf(ostEl.code) !== -1);
         if (fromOst) {
@@ -26,17 +27,35 @@ async function test() {
                 });
             }
         } else {
+            arr.push({
+                id: promEl.id,
+                name: promEl.name
+            });
             console.log(promEl.name);
         }
     });
     // console.log(diffGoods);
     // console.log(goods[0].id, goods[0].name, goods[0].presence, goods[0].price);
-    let count = 0;
-    while (count < diffGoods.length) {
-        await promClient.changeProductArray(diffGoods.slice(count, count + 100));
-        count += 100;
-    }
+    // let count = 0;
+    // while (count < diffGoods.length) {
+    //     await promClient.changeProductArray(diffGoods.slice(count, count + 100));
+    //     count += 100;
+    // }
     // await promClient.changeProductStatus(goods[0].id);
+    arr = arr.map(el => {
+        return {
+            id: el.id,
+            name: el.name.toString().replace('KН', 'KH')
+        };
+    });
+    // console.log(arr);
+    await promClient.changeProductArray(arr);
+
+    // let count = 0;
+    // while (count < arr.length) {
+    //     await promClient.changeProductArray(arr.slice(count, count + 100));
+    //     count += 100;
+    // }
 }
 
 test();
